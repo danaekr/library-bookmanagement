ee17b6f1b425c33dc3c125421e35a5a6beb0fe62
danaekr <117898830+danaekr@users.noreply.github.com>
2025-09-01 00:32:53 +0300

    Create UserDTO.java

---
+++ src/main/java/com/library/dto/UserDTO.java
@@ -0,0 +1,18 @@
+package com.library.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private Long id;
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String role;
+    private boolean active;
+}
