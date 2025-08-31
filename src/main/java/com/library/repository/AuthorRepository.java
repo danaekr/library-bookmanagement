0cbe0c03660cba923e7193caacc944c6570dd01b
danaekr <117898830+danaekr@users.noreply.github.com>
2025-09-01 00:46:09 +0300

    Create AuthorRepository.java

---
+++ src/main/java/com/library/repository/AuthorRepository.java
@@ -0,0 +1,11 @@
+package com.library.repository;
+
+import com.library.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+    List<Author> findByFirstNameContainingOrLastNameContaining(String firstName, String lastName);
+}
