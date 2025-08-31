ab691e3b3caa41a581749640557f9acc01b157dc
danaekr <117898830+danaekr@users.noreply.github.com>
2025-09-01 01:12:47 +0300

    Create CustomUserDetails.java

---
+++ src/main/java/com/library/security/CustomUserDetails.java
@@ -0,0 +1,72 @@
+package com.library.security;
+
+import com.library.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.Collections;
+
+public class CustomUserDetails implements UserDetails {
+    
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private Collection<? extends GrantedAuthority> authorities;
+    private boolean active;
+    
+    public CustomUserDetails(User user) {
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+        this.email = user.getEmail();
+        this.active = user.isActive();
+        this.authorities = Collections.singletonList(
+            new SimpleGrantedAuthority("ROLE_" + user.getRole().getName())
+        );
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+    
+    @Override
+    public String getPassword() {
+        return password;
+    }
+    
+    @Override
+    public String getUsername() {
+        return username;
+    }
+    
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    
+    @Override
+    public boolean isEnabled() {
+        return active;
+    }
+}
